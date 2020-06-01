485992ac65b91234a37bd1148735bc9a4a02f29a
antonzabreyko <anton.zabreyko@gmail.com>
2020-05-31 22:06:35 -0700

    Continued implementing skeleton

--- Gauss.py
+++ Gauss.py
@@ -13,22 +13,30 @@ class GaussElim(object):
         if not GaussElim.__prelims__(equations, solutions):
             return
 
+
+
     '''Ensures that the inputs are valid.'''
+    @staticmethod
     def __prelims__(equations, solutions):
         try:
             assert equations.shape[1] == solutions.shape[1]
         except AssertionError as e:
-            print("Mistmatch: {1} equation(s), {2} solution(s)".format(equations.shape[1]. solutions.shape[1]))
+            print("Mistmatch: {0} equation(s), {1} solution(s)".format(equations.shape[1], solutions.shape[1]))
             return False
         return True
 
-
+    '''For an NxM matrix, return an NxN matrix with all linearly independent rows/columns. '''
+    @staticmethod
+    def __ideal__(equations):
+        ideal_matrix = equations[0]
+        for i in range(1, ideal_matrix.shape[0]):
+            if np.linalg.lstsq()
 
 
 
 def test():
     equations = np.array([[1, 2], [5, 6]])
-    solutions = np.array([[3, 11]])
+    solutions = np.array([[3, 11, 10]])
     print(GaussElim.solveSystem(equations, solutions))
 
 test()
